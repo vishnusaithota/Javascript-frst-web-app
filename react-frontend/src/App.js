26a573b9bb1e85230cbb1d3b59ebad7deff309fc
tnvrravish <vt7@njit.edu>
2023-07-17 00:35:51 -0400

    added axios dependency for web communication to backen and also enabled proxy for requests to go through and then verified the output in console

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import axios from "axios"
 
 function App() {
+  axios.get("/api/v1/todos")
+    .then(res => {
+      console.log(res.data);
+    }).catch(err => {
+      console.log(err);
+    });
   return <h1> Hello React </h1>
 }
 
